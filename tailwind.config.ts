d4f23c9286b2465b0b92e0cb71d117e8350f5423
Gabriel Barros <ugabrieldev@gmail.com>
2024-08-29 09:52:12 -0300

    fix: overwrite other styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,7 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
@@ -16,5 +17,6 @@ const config: Config = {
     },
   },
   plugins: [],
+  important: true,
 };
 export default config;
