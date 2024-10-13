fc894ea1bf2ad4654acc5169c6111fb2fa840c2b
Gabriel Barros <ugabrieldev@gmail.com>
2024-10-13 12:16:53 -0300

    feat: add getMindMap function to fetch a specific mind map for a user

---
+++ src/services/getMindMap.ts
@@ -0,0 +1,26 @@
+
+import { MindMap } from "@/types/mind-map";
+import axios from "axios";
+
+export interface MindMapResponse {
+    id: string;
+    title: string;
+    mindMap: MindMap;
+    createdAt: string;
+    updatedAt: string;
+    userId: string;
+}
+
+export async function getMindMap(userId: string, mindMapId: string): Promise<MindMapResponse | null> {
+    try {
+        const { data, status } = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/mindmap/${mindMapId}/userId/${userId}`);
+
+        if (status === 200) {
+            return data;
+        }
+        return null
+    } catch (error) {
+        console.error(error);
+        return null
+    }
+}
