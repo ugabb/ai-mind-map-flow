f731dde941a05e39c7d20c6e5cf6b0b111fb14af
Gabriel Barros <ugabrieldev@gmail.com>
2024-10-13 18:32:18 -0300

    Refactor CardMindMap component to use MindMapResponse type

--- src/components/CardMindMap.tsx
+++ src/components/CardMindMap.tsx
@@ -1,17 +1,14 @@
 import Link from "next/link";
-import { format } from "date-fns";
 import { formatDateTime } from "@/utils/formatDate";
+import { MindMapResponse } from "@/types/mind-map";
 
 interface CardMindMapProps {
-  mindMap: {
-    id: string;
-    title: string;
-    lastUpdatedTime: string;
-  };
+  mindMap: MindMapResponse
 }
 
 export const CardMindMap = (props: CardMindMapProps) => {
   const { mindMap } = props;
+
   return (
     <Link
       href={`/mind-map/${mindMap.id}`}
@@ -20,7 +17,7 @@ export const CardMindMap = (props: CardMindMapProps) => {
       <div className="w-full h-40 bg-zinc-100 rounded-lg "></div>
       <div className="flex justify-between items-center">
         <h1 className="text-md font-medium">{mindMap.title}</h1>
-        <p className="text-xs text-foreground">{formatDateTime(mindMap.lastUpdatedTime)}</p>
+        <p className="text-xs text-foreground">{formatDateTime(mindMap.updatedAt)}</p>
       </div>
     </Link>
   );
