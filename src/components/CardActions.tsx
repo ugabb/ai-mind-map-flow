f0c0e525cbcd2f396e70b31195c3979abb01ac67
Gabriel Barros <ugabrieldev@gmail.com>
2024-10-17 08:36:20 -0300

    Refactor CardActions component to include additional HTML attributes

--- src/components/CardActions.tsx
+++ src/components/CardActions.tsx
@@ -1,14 +1,14 @@
 import { IconType } from "react-icons/lib";
 
-interface CardActionsProps {
+interface CardActionsProps extends React.HTMLAttributes<HTMLDivElement> {
   text: string
   icon: IconType
 }
 
 export const CardActions = (props: CardActionsProps) => {
-  const { icon: Icon, text } = props;
+  const { icon: Icon, text, ...rest } = props;
   return (
-    <div className="flex gap-3 items-center p-5 bg-zinc-100 text-md font-medium rounded-lg md:min-w-32 cursor-pointer hover:bg-indigo-500/10">
+    <div className="flex gap-3 items-center p-5 bg-zinc-100 text-md font-medium rounded-lg md:min-w-32 cursor-pointer hover:bg-indigo-500/10" {...rest}>
       {Icon && <Icon size={30} className="text-indigo-500" />}
       {text && <p className="hidden md:block">{text}</p>}
     </div>
