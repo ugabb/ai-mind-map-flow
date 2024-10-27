838fc0e72c27472dd7d1284569028c733da3a529
Gabriel Barros <ugabrieldev@gmail.com>
2024-10-27 12:22:18 -0300

    feat: add middleware to protect logged in routes

---
+++ src/middleware.ts
@@ -0,0 +1,40 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { cookies } from "next/headers";
+
+const loggedInRoutes = ["/home", '/mind-map'];
+const loggedOutRoutes = ["/login", "/sign-up"];
+
+
+export default async function AuthMiddleware(
+  req: NextRequest
+): Promise<NextResponse> {
+  if (
+    !loggedInRoutes.some((path) =>
+      req.nextUrl.pathname.startsWith(path)) &&
+    !loggedOutRoutes.some((path) => req.nextUrl.pathname.startsWith(path))
+  ) {
+    return NextResponse.next();
+  } else {
+    const myCookie = cookies();
+
+    let token: string | null = null;
+    if (myCookie.get("authjs.session-token")) {
+      token = myCookie.get("authjs.session-token")!.value;
+    }
+
+    if (
+      !token &&
+      loggedInRoutes.some((path) => req.nextUrl.pathname.startsWith(path))
+    ) {
+      return NextResponse.redirect("http://localhost:3000/login");
+    } else if (
+      token &&
+      loggedOutRoutes.some((path) => req.nextUrl.pathname.startsWith(path))
+    ) {
+      return NextResponse.redirect("http://localhost:3000/dashboard");
+    }
+  }
+
+  return NextResponse.next();
+}
