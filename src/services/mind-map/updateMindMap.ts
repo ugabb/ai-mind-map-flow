607ccd56099c7c90d06df50d1ea41a3e7a89517b
Gabriel Barros <ugabrieldev@gmail.com>
2025-09-11 20:46:28 -0300

    refactor: update updateMindMap function to use PUT method and improve code formatting

--- src/services/mind-map/updateMindMap.ts
+++ src/services/mind-map/updateMindMap.ts
@@ -2,34 +2,42 @@ import { Edge, Node, ReactFlowJsonObject } from "@xyflow/react";
 import { api } from "../axios";
 
 export interface MindMapResponse {
-    id: string;
-    title: string;
-    mindMap: ReactFlowJsonObject<Node, Edge>;
-    createdAt: string;
-    updatedAt: string;
-    userId: string;
+  id: string;
+  title: string;
+  mindMap: ReactFlowJsonObject<Node, Edge>;
+  createdAt: string;
+  updatedAt: string;
+  userId: string;
 }
 
 export interface UpdateMindMapRequest {
-    mindMap?: ReactFlowJsonObject<Node, Edge>;
-    title?: string;
-    userId: string;
-    mindMapId: string;
+  mindMap?: ReactFlowJsonObject<Node, Edge>;
+  title?: string;
+  userId: string;
+  mindMapId: string;
 }
 
-export async function updateMindMap({userId, mindMapId, title, mindMap}: UpdateMindMapRequest): Promise<MindMapResponse[]> {
-    try {
-        const { data, status } = await api.patch(`/update-mind-map/${mindMapId}/userId/${userId}`, {
-            ...(title && { title }),
-            mindMap
-        });
+export async function updateMindMap({
+  userId,
+  mindMapId,
+  title,
+  mindMap,
+}: UpdateMindMapRequest): Promise<MindMapResponse[]> {
+  try {
+    const { data, status } = await api.put(
+      `/update-mind-map/${mindMapId}/userId/${userId}`,
+      {
+        ...(title && { title }),
+        mindMap,
+      }
+    );
 
-        if (status === 200) {
-            return data;
-        }
-        return [];
-    } catch (error: any) {
-        console.error(error);
-        return [];
+    if (status === 200) {
+      return data;
     }
+    return [];
+  } catch (error: any) {
+    console.error(error);
+    return [];
+  }
 }
